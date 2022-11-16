1fafe3375d97e8b36b5b76dfb26c3b48256ab98f
Strayker <45916090+StraykerPL@users.noreply.github.com>
2022-11-16 23:40:46 +0100

    feat: Add Rules button component
    Only styling and click handling

---
+++ rock-paper-scissors-frontend/src/app/components/rules-button/rules-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-rules-button',
+	templateUrl: './rules-button.component.html',
+	styleUrls: ['./rules-button.component.scss'],
+})
+export class RulesButtonComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+
+	rulesButtonClicked(): void {}
+}
