78255425ba36678a71dc39a827716b48d1860b45
Strayker <45916090+StraykerPL@users.noreply.github.com>
2022-11-30 23:43:28 +0100

    refactor: Add Rules diagram to modal
    Register Rules Diagram in icon registry
    Add bolder font weight and change lower font weight value to default
    Change title header tag to bigger one

--- rock-paper-scissors-frontend/src/shared/consts/IconRegistryData.ts
+++ rock-paper-scissors-frontend/src/shared/consts/IconRegistryData.ts
@@ -5,4 +5,8 @@ export const IconRegistryData: Array<IconRegistryObject> = [
 		iconName: 'close-icon',
 		iconPath: '../../assets/images/icon-close.svg',
 	},
+	{
+		iconName: 'rules-diag',
+		iconPath: '../../assets/images/image-rules.svg',
+	},
 ];
